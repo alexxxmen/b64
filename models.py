5a8b1ee59ccd697d19119e555aa067dc97c1b614
PetruninAleksey <petrunin.aleksey.dev@gmail.com>
2017-06-02 00:02:47 +0300

    added Event model

--- models.py
+++ models.py
@@ -1,1 +1,27 @@
 # -*- coding:utf-8 -*-
+
+from peewee import Model, CharField, DateTimeField, TextField, datetime as peewee_datetime
+from playhouse.pool import PooledPostgresqlExtDatabase
+
+from config import DB_CONFIG
+
+peewee_now = peewee_datetime.datetime.now
+
+db = PooledPostgresqlExtDatabase(**DB_CONFIG)
+db.commit_select = True
+db.autorollback = True
+
+
+class _Model(Model):
+    class Meta:
+        database = db
+
+
+class Event(Model):
+    class Meta:
+        db_table = 'events'
+
+    title = CharField()
+    start = DateTimeField()
+    description = TextField(null=True)
+    created = DateTimeField(default=peewee_now)
