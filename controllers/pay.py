1a8e98b3a9067ee495f388583f832d67ac528705
PetruninAleksey <petrunin.aleksey.dev@gmail.com>
2017-10-03 23:56:16 +0300

    added pay protocol

--- controllers/pay.py
+++ controllers/pay.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
+import hashlib
 from flask import render_template
 
-from config import PAY_URL, SHOP_ID
+from config import PAY_URL, SHOP_ID, SHOP_SECRET
 from constants import OperationType
 from controllers import TemplateController, ServiceException, BidIDCoder
 
@@ -23,9 +24,15 @@ class PayController(TemplateController):
         data = dict(
             url=PAY_URL,
             shop_id=SHOP_ID,
-            partner_invoice_id=bid.id,
+            shop_invoice_id=bid.id,
             amount=bid.amount,
-            sign='',  # todo generate
             description=u"Оплата заявки № %s" % bid.id,
+            currency=840
         )
+        data['sign'] = self._get_sign(data, ("shop_id", "shop_invoice_id", "amount", "currency"), SHOP_SECRET)
         return render_template('pay/pay.html', data=data)
+
+    def _get_sign(self, request, keys_required, secret):
+        keys_sorted = sorted(keys_required)
+        string_to_sign = ":".join([unicode(request[k]).encode("utf8") for k in keys_sorted]) + secret
+        return hashlib.md5(string_to_sign).hexdigest()
